f1778d0f36fd8f9b6ec423cf67d87bc8b2ddab67
Calarasanu Andrei <fanfanixxx@gmail.com>
2021-01-16 19:03:50 +0200

    send data to service #5

--- client_api/go.mod
+++ client_api/go.mod
@@ -4,10 +4,13 @@ go 1.15
 
 require (
 	github.com/FanFani4/ports/ports v0.0.0-20210116161422-cb5a005e6754
+	github.com/andybalholm/brotli v1.0.1 // indirect
 	github.com/bcicen/jstream v1.0.1
 	github.com/joho/godotenv v1.3.0
+	github.com/klauspost/compress v1.11.7 // indirect
 	github.com/mitchellh/mapstructure v1.4.1
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.7.0
-	google.golang.org/grpc v1.35.0 // indirect
+	github.com/valyala/fasthttp v1.19.0
+	google.golang.org/grpc v1.35.0
 )
--- client_api/main.go
+++ client_api/main.go
@@ -2,17 +2,23 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
 
 	"github.com/FanFani4/ports/client_api/reader"
+	"github.com/FanFani4/ports/client_api/sender"
+	"github.com/FanFani4/ports/ports"
 	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
+	"github.com/valyala/fasthttp"
+	"google.golang.org/grpc"
 )
 
 func main() {
 	log := logrus.New()
-	ctx := context.TODO()
+	ctx, cancel := context.WithCancel(context.Background())
 
 	err := godotenv.Load()
 	if err != nil {
@@ -28,7 +34,54 @@ func main() {
 
 	portsResponse := reader.GetPorts()
 
-	for port := range portsResponse {
-		fmt.Println(port)
+	go runSync(log, portsResponse)
+
+	port := os.Getenv("PORT")
+	if _, err = strconv.Atoi(port); err != nil {
+		log.Fatal("PORT must be an int")
 	}
+
+	host := os.Getenv("HOST")
+	if host == "" {
+		log.Fatal("HOST is required")
+	}
+
+	s := &fasthttp.Server{
+		Logger: log,
+	}
+
+	go func() {
+		log.Info("Server running on: http://" + host + ":" + port)
+		log.Info(s.ListenAndServe(host + ":" + port))
+	}()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGTERM)
+	signal.Notify(c, syscall.SIGINT)
+
+	<-c
+
+	log.Info("sync shutdown")
+	cancel()
+
+	log.Info("Start server shutting down")
+
+	err = s.Shutdown()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("\nShutting down the server...")
+}
+
+func runSync(log *logrus.Logger, portsResponse <-chan *ports.Port) {
+	conn, err := grpc.Dial(os.Getenv("SERVER_ADDR"), grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+
+	cli := ports.NewPortDomainServiceClient(conn)
+	sender := sender.NewSender(context.Background(), log, cli)
+
+	sender.Send(portsResponse)
 }
